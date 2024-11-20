23aa6a93c99f0561e832b0c08d9f1d44ee7633a8
opilane <opilane@HP-CZC2349HV1>
2024-11-20 16:41:03 +0200

    143

--- TARgv24_Python.py
+++ TARgv24_Python.py
@@ -1,1 +1,37 @@
-print("Tere tulemast!")
+from random import * #*-kõik funktsioonid, randint as rd funktsioonide ümbernimetus
+
+from math import * #pi kasutamiseks
+
+#ülesanne 1
+# print("Tere tulemast!")
+# nimi = input("Mis on sinu nimi?").capitalize() #lower()-aaa, upper()-AAA, capitalize()-Aaa
+# print("Tere tulemast! Tervitan sind ", nimi)
+# print("Tere tulemast! Tervitan sind "+ nimi)
+# vanus = int(input("Kui vana sa oled? "))
+# print("Tere tulemast! Tervitan sind  "+ nimi + " Sa oled ", vanus, "aastat vana")
+# print(f"\tTere tulemast! \nTervitan sind {nimi} Sa oled {vanus} aastat vana")
+
+#ülesanne 2
+# vanus = 18
+# eesnimi = "Jaak"
+# pikkus = 16.5
+# kas_käib_koolis = True
+# print(type(vanus))
+# print(type(eesnimi))
+# print(type(pikkus))
+# print(type(kas_käib_koolis))
+
+#ülesanne 3
+# kokku = randint(1, 1000)
+# print(f"Kokku on {kokku} kommi")
+# kommi = int(input("Mitu kommi sa tahad? "))
+# kokku = kokku - kommi
+# print(f"Jääk on {kokku} kommi")
+
+#ülesanne 4
+print("Läbimõõdu leidmine ")
+#l-ümbermõõt
+l = float(input("Ümbermõõt: "))
+d = l/pi
+print(f"Lääbimõõdu suurus on {round(d,2)}")
+print('test')
