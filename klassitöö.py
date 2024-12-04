1346d5c2dac9487b88de41b36da53d99ff83a107
admin <admin@DESKTOP-RMGD1UN>
2024-12-04 15:44:10 +0200

    kodutöö

--- klassitöö.py
+++ klassitöö.py
@@ -3,7 +3,7 @@ from calendar import *
 from random import *
 from math import *
 
-ülesanne 1
+#ülesanne 1
 päevadekogus = monthrange(2024,11)[1] #2024 - нулевая позиция и 11 - 1-ая позиция/calendar modulist
 print(monthrange(2024,11)[1])
 
@@ -24,15 +24,15 @@ jaak2 = novP - d
 print(f"Aasta loppuni on {jaak}")
 print(f"Kuu loppuni on {jaak2}")
 
-ülesanne 2
+#ülesanne 2
 vastus1 = 3 + 8 / (4 - 2) * 4
 vastus2 = 3 + 8 / 4 - 2 * 4
 vastus3 = (3 + 8) / (4 - 2) * 4
 print(vastus1,"\n",vastus2,"\n",vastus3)
 
-ülesanne 3
+#ülesanne 3
 
-1 variant
+#1 variant
 try:
     r = float(input("Sisesta R: "))
     Sk = pi*r**2 #площадь круга
@@ -43,7 +43,7 @@ try:
 except:
     print("On vaja number!")
 
-2 variant
+#2 variant
 r = int(random()*100) #0.0...1.0
 print(f"r = {r}")
 Sk = pi*r**2 #площадь круга
@@ -52,7 +52,7 @@ Skv = (2*r)**2
 Lkv = 2*r*4
 print(f"Ringi pindala on {Sk}\nRingi umbermoot on {Lk}\nRuudu pindala on {Skv}\nRuudu umbermoot on {Lkv}")
 
-ülesanne 4
+#ülesanne 4
 D = 2.575 #mundi d
 maa = 6378 #maa radius km
 maa *= 100000 #maa raduis cm + maa=maa*100000
@@ -60,12 +60,12 @@ Lmaa = 2*pi*maa
 kogus = int(Lmaa/D)
 print(f"On vaja {kogus} mundi.\nMeil on vaja {kogus*2} eur")
 
-ülesanne 5
+#ülesanne 5
 a = "kill-koll ".capitalize()
 b = "killadi-koll ".capitalize()
 print(a*2, b, a*2, b, a*4)
 
-ülesanne 6
+#ülesanne 6
 a = """Rong see sõitis tsuhh tsuhh tsuhh,
 piilupart oli rongijuht.
 Rattad tegid rat tat taa,
@@ -82,7 +82,7 @@ print(a)
 print()
 print(b)
 
-ülesanne 7
+#ülesanne 7
 d = float(input("длина прямоугольника: "))
 s = float(input("ширина прямоугольника: "))
 pr = 2*(d + s)
@@ -90,19 +90,19 @@ pl = d * s
 print(f"периметр прямоугольника: {pr}")
 print(f"площадь прямоугольника: {pl}")
 
-ülesanne 8
+#ülesanne 8
 t = float(input("количество литров топлива в баке: "))
 k = float(input("пройденные километры: "))
 sr = t/k * 100
 print(round(sr, 2))
 
-ülesanne 9
+#ülesanne 9
 print("Средняя скорость составляет 29.9 км/ч.")
 m = int(input("Время в минутах: "))
 rasstojanie = 29.9 * (m / 60)
 print(f"За {m} минут(ы) роллер преодолеет {round(rasstojanie, 2)} км.")
 
-ülesanne 10
+#ülesanne 10
 m = int(input("время в минутах: "))
 hours = m//60
 rm = m % 60
--- vigade.otsing.py
+++ vigade.otsing.py
@@ -28,14 +28,14 @@
 # print("Ringjoone pikkus", round(C)
 
 #правильный порядок
-from math import *
-print("Ruudu karakteristikud")
-a = float(input('Sisesta ruudu külje pikkus => '))
+from math import * #правильный импорт
+print("Ruudu karakteristikud") 
+a = float(input('Sisesta ruudu külje pikkus => ')) #ввод в число с плавающей точкой
 S = a**2
 print("Ruudu pindala:", S)
 P = 4*a
 print("Ruudu ümbermõõt:", P)
-di = a*math.sqrt(2)
+di = a * sqrt(2)
 print("Ruudu diagonaal:", round(di, 2))
 print()
 print("Ristküliku karakteristikud")
@@ -45,14 +45,14 @@ S = b*c
 print("Ristküliku pindala:", S)
 P = 2 * (b+c)
 print("Ristküliku ümbermõõt:", P)
-di = math.sqrt(b**2+c**2)
+di = sqrt(b**2+c**2)
 print("Ristküliku diagonaal:", round(di, 2))
 print()
 print("Ringi karakteristikud")
 r = float(input('Sisesta ringi raadiusi pikkus => '))
 d = 2 * r
 print("Ringi läbimõõt:", d)
-S = math.pi*r**2
+S = pi*r**2
 print("Ringi pindala:", round(S, 2))
-C = 2 * math.pi * r
+C = 2 * pi * r
 print("Ringjoone pikkus:", round(C, 2))
