2af6e63bf6adc4115095448baa7bc86eb48353fd
admin <admin@DESKTOP-RMGD1UN>
2024-12-14 18:26:38 +0200

    kodutöö

---
+++ for.while.true.py
@@ -0,0 +1,58 @@
+#ulesanne 2
+# while True:
+#     try:
+#         A = int(input("Sisesta A:"))
+#         break
+#     except:
+#         print("On vaja naturaalne arv")
+# summa = 0
+# if A>0:
+#     for i in range(1,A+1,1):
+#         summa+=i            #summa=summa+1
+#         print(f"{i}. samm summa={summa}")
+#     print(f"Vastus {summa}")
+
+
+#ulesanne 3
+# p = 1
+# for j in range(8):
+#     while True:
+#         try:
+#             arv = float(input(f"Sisesta {j+1} arv:"))
+#             break
+#         except:
+#             print("On vaja arv")
+#     if arv>0: 
+#         p*=arv
+#     else:
+#         print("Korrutame arvud rohkem kui 0")
+#     print(f"{j}. samm korrutis= {p}")
+# print(f"Lopptulemus on {p}")
+
+
+#ulesanne 4
+# for n in range(10,21,1):
+#     print(n**2, end=";")
+
+# print()
+
+#ulesanne 16
+# for j in range(1,10):
+#     for i in range(1,10):
+#         if i==j:
+#             print(j, end=" ")
+#         else:
+#             print("0", end=" ")
+#     print()
+
+
+#ulesanne 15
+# for read in range(10):
+#     for rida in range(10):
+#         print(rida, end=" ")
+#     print()
+# print()
+
+#ulesanne 1
+
+
