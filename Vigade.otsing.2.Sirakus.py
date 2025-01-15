e377f1124d58589a410f0a2ad54516e52e8b68c1
admin <admin@DESKTOP-RMGD1UN>
2025-01-15 14:28:53 +0200

    listid

---
+++ Vigade.otsing.2.Sirakus.py
@@ -0,0 +1,58 @@
+﻿
+print("*** ARVUDE MÄNG ***")
+print()
+#'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+while 1:
+    try:
+        a = abs(int(input("Siseta täisarv => ")))
+        break
+    except ValueError:
+        print("See ei ole täisarv")
+#'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+if a==0:
+    print("Nulliga on mõttetu töö")
+else:
+#'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    print("Loendame, mitu on paaris ja mitu paaritu arvu")
+    print()
+    c=b=a
+    paaris=0
+    paaritu=0
+    while b > 0:
+        if b % 2 == 0:   #остаток от деления на ноль
+            paaris += 1  #если =+, то это приравнивание
+        else:
+            paaritu += 1
+        b = b // 10 #оставляет целую часть
+    
+    print("Paaris arv:", paaris)
+    print("Paaritu arv:", paaritu)
+    print()
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    print("*Ümberpöörame* sisestatud arv")
+    print()
+    b=0
+    while a > 0:
+        number = a % 10
+        a = a // 10
+        b = b * 10
+        b += number
+    print("*Ümberpööratud* arv", b)
+    print()
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    print("Tõestame teoreem")
+    print()
+    # if c % 2 == 0:
+    #     print(c, " - paaris arv. jagame 2.")
+    # else:
+    #     print(c, " - paaritu arv. korrutame 3, liidame 1 ja jagame 2.")
+    while c != 1:
+        if c % 2 == 0:
+            print('{:>4}'.format(round(c))," - paaris arv, jagame 2.")
+            c = c / 2
+    else:
+        print('{:>4}'.format(round(c))," - paaritu arv. korrutame 3, liidame 1 ja jagame 2.")
+        c = (3*c + 1) / 2
+
+    print('{:>4}'.format(round(c))," - Teoreem on toestatud")
+    print()
---
+++ plokkskeem.py
@@ -0,0 +1,16 @@
+﻿
+isiku_kood = input("Введите свой личный код: ")
+kokku_mehed = 0
+kokku_naised = 0
+esimene_number = isiku_kood[0]
+print(f"Первая цифра личного кода: {esimene_number}")
+
+if esimene_number in [1, 3, 5, 7, 9]:
+    kokku_mehed +=1
+    print("Мужчина")
+
+if esimene_number in [2, 4, 6, 8]:
+    kokku_naised +=1
+    print("Женщина")
+
+
