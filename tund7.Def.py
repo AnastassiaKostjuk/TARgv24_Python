61639583dc84d92d0dddee97325f863ad32bb621
admin <admin@DESKTOP-RMGD1UN>
2025-01-22 16:30:27 +0200

    tund7.def.

--- tund7.Def.py
+++ tund7.Def.py
@@ -1,4 +1,5 @@
-# from tund7Module import *
+﻿from tund7Module import *
+#ulesanne
 # a=int(input("Sisesta arv1: "))
 # b=int(input("Sisesta arv2: "))
 # c=input("Sisesta arv3: ")
@@ -6,4 +7,10 @@
 # vastus=summa3(a,b,int(c))
 # print(vastus)
 
+#ulesanne4
+# month = int(input("Введите номер месяца (1-12): "))
+# print(season(month))
 
+#ulesanne6
+number=float(input("Введите число от 0 до 1000: "))
+print(is_prime)
--- tund7Module.py
+++ tund7Module.py
@@ -1,4 +1,5 @@
-﻿#ulesanne1
+﻿from random import *
+#ulesanne
 # def summa3(arv1:int, arv2:int, arv3:int)->int:
 #     """Tagastab kolme taisarvu summa
 
@@ -11,7 +12,7 @@
 #     summa=arv1+arv2+arv3
 #     return summa
 
-#ulesanne2
+#ulesanne1
 # def arithmetic(a:float, b:float,t:str)->any:
 #     if t in i["+","-","+","/"]:
 #         if b==0 and t=="/":
@@ -22,10 +23,52 @@
 #         vastus("Tundmatu tehe!")
 #     return vastus
 
+# #ulesanne2
+# def is_year_leap(aasta:int)->bool:
+#     if aasta%4==0:
+#         v=True
+#     else:
+#         v=False
+#     return v
+
 #ulesanne3
-def is_year_leap(aasta:int)->bool:
-    if aasta%4==0:
-        v=True
-    else:
-        v=False
+# def square(a:float)->any:
+#     S=a**2
+#     P=4*a
+#     d=(2)**(1/2)*a
+#     return S,P,d
+
+#ulesanne4
+# def season(month:int)->str:
+#     if month in [1,2,12]:
+#         season="talv"
+#     elif month in [3,4,5]:
+#         season="kevad"
+#     elif month in [6,7,8]:
+#         season="suvi"
+#     elif month in [9,10,11]:
+#         season="sugis"
+
+#     return season
+
+#ulesanne5
+# def bank(a:float,years:int)->float:
+#     for i in range(years):
+#         a*= 0.1  # Увеличиваем вклад на 10%
+#     return a
+
+#ulesanne6
+def is_prime(a=randint(0,1000))->bool:
+    print(a)
+    v=True
+    for i in range(2,a):
+        if a%i==0:
+            v=False
     return v
+
+#ulesanne 8
+# def XOR_cipher(text:str, voti:int)->str:
+#     kodeeritud=""
+#     for symbol in text:
+#         kodeeritud+=chr(ord(symbol)^voti)
+#     return kodeeritud
