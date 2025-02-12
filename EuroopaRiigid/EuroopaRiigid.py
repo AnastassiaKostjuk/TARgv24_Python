f51fc1ede67d7933a7309011cad9455fca0352a1
admin <admin@DESKTOP-RMGD1UN>
2025-02-12 13:15:08 +0200

    riigid

---
+++ EuroopaRiigid/EuroopaRiigid.py
@@ -0,0 +1,21 @@
+from random import *
+def failist_to_dict(f:str):
+    riik_pealinn={}
+    pealinn_riik={}
+    riigid=[]
+    file=open(f,'r',encoding="utf-8-sig")
+    for line in file:
+        k,v=line.strip().split('-')
+        riik_pealinn[k]=v
+        pealinn_riik[v]=k
+        riigid.append(k)
+    file.close()
+    return riik_pealinn, pealinn_riik, riigid
+
+riik_pealinn, pealinn_riik, riigid=failist_to_dict("riigid_pealinnad.txt")
+riigid=list(riik_pealinn.keys())
+print(riigid)
+print(riik_pealinn.keys())
+pealinnad=list(riik_pealinn.values())
+print(pealinnad)
+print(riik_pealinn)
